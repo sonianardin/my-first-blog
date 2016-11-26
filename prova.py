ac50b60b95b2183036d2c34551166500a964be0f
Sonia <nardin.sonia@gmail.com>
2016-11-26 13:50:42 +0100

    La mia app Django Girls, primo commit

---
+++ prova.py
@@ -0,0 +1,8 @@
+def ciao(nome):
+  if nome == 'Ola':
+    print('Ciao Ola!')
+  elif nome == 'Sonja':
+    print('Ciao Sonja!')
+  else:
+    print('Ciao anonimo!')
+ciao("Ola")
---
+++ prova_intro.py
@@ -0,0 +1,13 @@
+ragazze = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Tu']
+for nome in ragazze:
+    def ciao(nome):
+       print('Ciao ' + nome + '!')
+
+ragazze = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Tu']
+for nome in ragazze:
+    ciao(nome)
+    print('Prossima ragazza')
+
+for i in range(1,6):
+    print (i)
+    
